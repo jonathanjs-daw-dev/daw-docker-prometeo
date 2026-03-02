0a0394938c785a976445de022df747441605eb6f
Jonathan <jonathan.luis.jimenez.salazar@students.thepower.education>
2026-03-02 11:24:12 +0000

    fix(trigger): Add DELIMITER 15312 text line

--- sql/trigger.sql
+++ sql/trigger.sql
@@ -1,10 +1,12 @@
 USE prometeo;
 
+DELIMITER $$
+
 CREATE TRIGGER trg_email_no_vacio
 BEFORE INSERT ON clientes
 FOR EACH ROW
 BEGIN
-  IF NEW.email IS NULL OR NEW.email = "" THEN
+  IF NEW.email IS NULL OR NEW.email = '' THEN
     SIGNAL SQLSTATE '45000'
     SET MESSAGE_TEXT = "El email no puede estar vacio";
   END IF;
