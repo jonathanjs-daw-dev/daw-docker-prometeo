09a06fb13ad4e6defdc57ec4ce49a7f0990d6210
Jonathan <jonathan.luis.jimenez.salazar@students.thepower.education>
2026-03-02 11:27:52 +0000

    fix: pls work

--- sql/trigger.sql
+++ sql/trigger.sql
@@ -8,8 +8,8 @@ FOR EACH ROW
 BEGIN
   IF NEW.email IS NULL OR NEW.email = '' THEN
     SIGNAL SQLSTATE '45000'
-    SET MESSAGE_TEXT = "El email no puede estar vacio";
+    SET MESSAGE_TEXT = 'El email no puede estar vacio';
   END IF;
 END$$
 
-DELIMITER;
+DELIMITER ;
